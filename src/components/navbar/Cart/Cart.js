2c9dcc7c5a2a396b46377c15936e551c6061f18e
luvuyo <luvuyotafeni012@gmail.com>
2024-03-15 23:57:18 +0200

    adding the cart overlay

--- src/components/navbar/Cart/Cart.js
+++ src/components/navbar/Cart/Cart.js
@@ -1,15 +1,14 @@
 // Cart.js
-
 import React from 'react';
 import './Cart.css';
 
 const Cart = ({ onClose }) => {
   return (
     <div className="cart-overlay">
+      
       <div className="cart">
         <button className="close-cart" onClick={onClose}><i className="bx bx-x"></i></button>
-        <h2>Your Cart</h2>
-        {/* Add your cart content here */}
+        <h2>Your Cart is empty</h2>
       </div>
     </div>
   );
--- src/components/navbar/LoginPopup.js
+++ src/components/navbar/LoginPopup.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const LoginPopup = ({ onClose }) => {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const handleToggleForm = () => {
+    setIsLogin(!isLogin);
+  };
+
   return (
     <div className='login-popup'>
       <div className='popup-content'>
-        <h2>Login</h2>
+        <h2>{isLogin ? 'Login' : 'Sign Up'}</h2>
         <form>
             <div>
                 <div>           
@@ -18,12 +24,27 @@ const LoginPopup = ({ onClose }) => {
                 </div>
                 <input type='text' className='logIn_input'></input>
             </div>
-          <button type='submit'>Login</button>
+            {!isLogin && (
+              <div>
+                <div>
+                  <label>Email</label>
+                </div>
+                <input type='email' className='logIn_input'></input>
+              </div>
+            )}
+            <div>
+              {isLogin ? (
+                <a href='#' onClick={handleToggleForm} className='login_link'>Don't have an account? Sign Up</a>
+              ) : (
+                <a href='#' onClick={handleToggleForm} className='login_link'>Already have an account? Login</a>
+              )}
+            </div>
+          <button type='submit'>{isLogin ? 'Login' : 'Sign Up'}</button>
         </form>
         <button onClick={onClose}>Close</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPopup
+export default LoginPopup;
