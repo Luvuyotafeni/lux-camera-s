868ab405e41dc40c8f64cf9545e4ac3335999b40
luvuyo <luvuyotafeni012@gmail.com>
2024-03-15 22:55:07 +0200

    fully added products

--- src/components/Blog/products_api.js
+++ src/components/Blog/products_api.js
@@ -6,6 +6,8 @@ import image5 from "./assets/canonR1018to150mmKit.png"
 import image6 from "./assets/canon eOS 700D.jpg"
 import image7 from "./assets/Caon EOS R5.jpg"
 import image8 from "./assets/CANON EOS-1D X MARK III.jpg"
+import image9 from "./assets/Nikon COOLPIXP1000-z.jpg"
+import image10 from "./assets/Nikon D5500 DSLR.jpg"
 
 const data = [
     {
@@ -58,35 +60,35 @@ const data = [
       rating: 4.5,
     },
     {
-      id: 6,
+      id: 7,
       image: image7,
-      title: "Caon EOS R5",
+      title: "Canon EOS R5",
       desc: "45 megapixel resolution, Full Frame 4K/120P video, 20 frames per second, New IBIS HIGH RESOLUTION,CFexpress and SD card slots",
-      price: 79233.99,
+      price: 79895.00,
       rating: 4.5,
     },
     {
-      id: 6,
+      id: 8,
       image: image8,
       title: "CANON EOS-1D X MARK III",
       desc: "20 megapixel full-frame senso, 5.5K Raw video, DIGIC X image processor, Dual Pixel CMOS AF",
-      price: 79233.99,
+      price: 86767.00,
       rating: 4.5,
     },
     {
-      id: 6,
-      image: image6,
-      title: "canonR1018",
-      desc: "18 megapixel CMOS, Great low light shots, High speed shooting, Continuous AF in Movies,Stunning 1080p movies",
-      price: 79233.99,
+      id: 9,
+      image: image9,
+      title: "Nikon COOLPIX",
+      desc: "16MP BSI CMOS Sensor, EXPEED Image Processor, 2.36m-Dot OLED EVF, 3.2″ 921k-Dot Vari-Angle LCD Monitor",
+      price: 35649.00,
       rating: 4.5,
     },
     {
-      id: 6,
-      image: image6,
+      id: 10,
+      image: image10,
       title: "canonR1018",
       desc: "18 megapixel CMOS, Great low light shots, High speed shooting, Continuous AF in Movies,Stunning 1080p movies",
-      price: 79233.99,
+      price: 20495.50,
       rating: 4.5,
     },
   ]
